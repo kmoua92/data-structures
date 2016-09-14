3d7fc0df4ebd34863aa8a9fd284a52298eea8e2a
kmoua92 <kermoua92@gmail.com>
2016-09-14 16:29:40 -0700

    Make queue functional instantiation pass all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var storageLength = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[storageLength] = value;
+    storageLength++;
   };
 
   someInstance.dequeue = function() {
+   
+    if (storageLength > 0) {
+      var deQueued = storage[0];
+      
+      delete storage[0];
+
+      storageLength--;
+      
+      for (var i = 0; i < storageLength; i++) {
+        storage[i] = storage[i + 1];
+      }
+    }
+
+    return deQueued;
+
+
   };
 
   someInstance.size = function() {
+    return storageLength;
   };
 
   return someInstance;
