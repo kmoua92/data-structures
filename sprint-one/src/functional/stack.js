fd5ae5fefc692015bfda9fefd93a0d941981bfc7
kmoua92 <kermoua92@gmail.com>
2016-09-14 16:04:28 -0700

    Make functional pattern stack pass all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var storageLength = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[storageLength] = value;
+    storageLength++;
   };
 
   someInstance.pop = function() {
+
+    if (storageLength > 0) {
+      storageLength--;
+      var popped = storage[storageLength];
+      delete storage[storageLength];
+    }
+    return popped;
   };
 
   someInstance.size = function() {
+    return storageLength;
   };
+
 
   return someInstance;
 };
