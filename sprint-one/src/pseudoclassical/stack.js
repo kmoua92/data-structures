2cbffa2de480bdb9d15f10d8c9110c57578e8249
kmoua92 <kermoua92@gmail.com>
2016-09-14 20:54:39 -0700

    Make pseudo classical stack and queue tests pass

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.sLength = 0;
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value) {
+  this.storage[this.sLength] = value;
+  this.sLength++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.sLength > 0) {
+    this.sLength--;
+    var popped = this.storage[this.sLength];
+    delete this.storage[this.sLength];
+    return popped;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.sLength;
+};
+
 
 
