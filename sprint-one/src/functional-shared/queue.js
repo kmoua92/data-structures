e781f5f5046f704e09e85e97f0ddd5f18cd1f933
kmoua92 <kermoua92@gmail.com>
2016-09-14 20:21:16 -0700

    Make functional shared queue and stack tests pass

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    sLength: 0,
+    storage: {}
+  };
+  
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.sLength;
+  },
+  enqueue: function(value) {
+    this.storage[this.sLength] = value;
+    this.sLength++;
+  },
+  dequeue: function() {
+    if (this.sLength > 0) {
+      this.sLength--;
+      var deQueue = this.storage[0];
+      delete this.storage[0];
+      
+      for (var i = 0; i < this.sLength; i++) {
+        this.storage[i] = this.storage[i + 1];
+      }
+
+      return deQueue;
+    }
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+
+  // var empty object for methods
+  // add pop, push, & size to methods
+
+  // create class
+    //extend methods  
+  var someInstance = {
+    sLength: 0,
+    storage: {}
+  };
+  console.log('Find');
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.sLength] = value;
+    this.sLength++;
+  },
+  size: function() {
+    return this.sLength;
+  },
+  pop: function() {
+    if (this.sLength > 0) {
+      this.sLength--;
+      var popped = this.storage[this.sLength];
+      delete this.storage[this.sLength];
+      return popped;
+    }
+  }
+};
 
 
