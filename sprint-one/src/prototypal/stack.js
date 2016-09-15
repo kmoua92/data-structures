beef747b26588d8b24ac2cc29e558b6aa3f73dab
kmoua92 <kermoua92@gmail.com>
2016-09-14 20:32:05 -0700

    Make prototypal pattern stack and queue tests pass

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.sLength = 0;
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.sLength] = value;
+    this.sLength++;
+  },
+  size: function() {
+    return this.sLength;
+  },
+  pop: function() {
+    if (this.sLength > 0) {
+      this.sLength--;
+      var popped = this.storage[this.sLength];
+      delete this.storage[this.sLength];
+      return popped;
+    }
+  }
+};
 
 
