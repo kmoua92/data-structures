7d809426eee38a4281d3ba204a0f2d055909d63b
kmoua92 <kermoua92@gmail.com>
2016-09-15 16:15:13 -0700

    Make graph with edges stored as array

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,30 +2,44 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  //this.nodeKey = 0;
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  //this.nodeKey++;
+  this[node] = {value: node, edge: []};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+
+  if (this[node]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return _.contains(this[fromNode].edge, toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  console.log(this[fromNode]);
+  this[fromNode].edge.push(toNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  // splice toNode from this[fromNode].edge
 };
 
 // Pass in a callback which will be executed on each node of the graph.
