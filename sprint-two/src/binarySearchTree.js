b8f5fb6b82aacb5a581a584a5a008d45bafdb6c5
kmoua92 <kermoua92@gmail.com>
2016-09-16 12:19:10 -0700

    Implement insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,33 @@
-var BinarySearchTree = function(value) {
+var BST = function(value) {
+  this.value = value;
+  this.left;
+  this.right;
 };
+
+BST.prototype.insert = function(value) {
+  if (!this.right && value > this.value) {
+    this.right = new BST(value);
+  } 
+
+  if (!this.left && value < this.value) {
+    this.left = new BST(value);
+  } 
+
+  if (this.right && value > this.value) {
+    this.right.insert(value);
+  }
+
+  if (this.left && value < this.value) {
+    this.left.insert(value);
+  }
+};
+
+BST.prototype.contains = function(value) {
+  // returns a boolean
+
+};
+
+BST.prototype.depthFirstLog = function() {};
 
 
 /*
