57537bdc02be2588fba63fe074dbe1dd7a9b4319
kmoua92 <kermoua92@gmail.com>
2016-09-17 17:32:13 -0700

    Implement resize function to handle doubling

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,12 +2,20 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this.count = 0;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(key, value) {
   var index = getIndexBelowMaxForKey(key, this._limit);
+  console.log('limit: ' + this._limit);
+  console.log('index: ' + index);
   var bucket = this._storage.get(index);
+
+  // check to double
+  if ((this.count + 1) / this._limit >= .75) {
+    this.resize(this._limit * 2);
+  }
 
   if (!bucket) {
     bucket = [];
@@ -16,6 +24,7 @@ HashTable.prototype.insert = function(key, value) {
   bucket.push([key, value]);
 
   this._storage.set(index, bucket);
+  this.count++;
 };
 
 HashTable.prototype.retrieve = function(key) {
@@ -23,7 +32,7 @@ HashTable.prototype.retrieve = function(key) {
   var bucket = this._storage.get(index);
   var tupleValue;
 
-  
+
   return _.reduce(bucket, function(tupleVal, tuple) {
     if (tuple[0] === key) {
       tupleVal = tuple[1];
@@ -38,13 +47,65 @@ HashTable.prototype.remove = function(key) {
   var bucket = this._storage.get(index);
   var newBucket = [];
 
+  // check to halve
+  if ((this.count - 1) / this._limit < .25) {
+    this.count = 0;
+    this.resize(this._limit / 2);
+
+  }
+
   _.each(bucket, function(tuple) {
     if (tuple[0] !== key) {
       newBucket.push([tuple[0], tuple[1]]);
     }
   });
 
-  this._storage.set(index, newBucket);  
+  this._storage.set(index, newBucket);
+  this.count--;  
+};
+
+HashTable.prototype.resize = function(newSize) {
+  var oldStorage = this._storage;
+  this._limit = newSize;
+  this._storage = LimitedArray(this._limit);
+
+  // declare holder array
+  var valHold = [];
+
+  // loop through storage
+  for (var i = 0; i < oldStorage.length; i++) {
+    var bucket = oldStorage[i];
+    // for each bucket, loop
+    for (var o = 0; o < bucket.length; o++) {
+      // push each tuple to holder
+      valHold.push(bucket[o]);
+    }
+  }
+
+  // loop through holder
+  for (var i = 0; i < valHold.length; i++) {
+    // insert tuple in new storage
+    // this.count = 0;
+    console.log('loop: ' + i);
+    this.insert(valHold[i][0], valHold[i][1]);
+  }
+  
+
+  // for (var i = 0; i < oldStorage.length; i++) {
+  //   var oldBucket = oldStorage[i];
+  //   if (oldBucket) {
+  //     var newBucket = [];
+  //     for (var o = 0; o < oldBucket.length; o++) {
+  //       var key = oldBucket[o][0];
+  //       var value = oldBucket[o][1];
+  //       // this.insert(key, value);
+  //       newBucket.push([key, value]);
+  //     }
+  //     this._storage.set(index, newBucket);
+  //   }
+  // }
+  
+  
 };
 
 
